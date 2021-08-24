183ece3e2e078afbec0146e3f7d9b68e1c3af9d5
The Gravener <ryan@wmwm.us>
2021-08-24 12:09:55 -0400

    re semantic

--- tf_semantic_segmentation/datasets/reported.py
+++ tf_semantic_segmentation/datasets/reported.py
@@ -29,9 +29,9 @@ class ReportedDS(Dataset):
         images = list(filter(lambda x: not x.endswith(".png"), imgs))
         labels = list(filter(lambda x: x.endswith(".png"), imgs))
         trainset = list(zip(images, labels))
-        t = get_split(trainset,val_split=1.0,shuffle=False)
-        v = get_split(trainset,val_split=.5,shuffle=False)
-        t = get_split(trainset,val_split=.3,shuffle=False)
+        t = get_split(trainset,train_split=1.0)
+        v = get_split(trainset,train_split=.5,shuffle=False)
+        t = get_split(trainset,train_split=.3,shuffle=False)
         
         return {
             DataType.TRAIN: t[DataType.TRAIN],
